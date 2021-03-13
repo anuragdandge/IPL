82f04a64a0f465b811f7374c6c64ce5db6682559
anuragdandge <41123540+anuragdandge@users.noreply.github.com>
2021-03-13 18:04:55 +0530

    Add files via upload

---
+++ UserData.js
@@ -0,0 +1,44 @@
+var db= firebase.firestore();
+function DisplayData(){
+    db.collection('users').get().then((snapshot)=>{
+        const users=[];
+        snapshot.forEach((docs)=>{
+          const data =docs.data();
+          users.push(data);
+          users.map((d)=>{
+              var name = d.UserName;
+              var numb = d.MobileNumber; 
+              addItems(name,numb)
+          })
+
+        })
+    });
+    
+}
+
+function addItems(name,numb){
+
+    var tbody = document.getElementById("tableBody")
+
+    var trow = document.createElement("tr")
+    
+    var td1 = document.createElement("td");
+    var td2 = document.createElement("td");
+
+    td1.innerHTML = name;
+    td2.innerHTML = numb;
+
+    trow.appendChild(td1);
+    trow.appendChild(td2);
+
+    tbody.appendChild(trow);
+
+
+    
+}
+
+
+
+
+
+
